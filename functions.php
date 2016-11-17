b2fda15358aaac1e1aa37d7e67f05fd1681e43bc
Xennis <code@xennis.org>
2016-11-17 19:58:38 +0100

    Deactivate sidebar for mobile devices
    Sidebar widgets are not useful for mobile devices.

--- functions.php
+++ functions.php
@@ -1,7 +1,10 @@
 <?php
-// Set local for correct dates
+// Set local for correct dates.
 setlocale(LC_ALL, get_locale().'.'.get_bloginfo('charset'));
 
+/**
+ * Include styles and scripts.
+ */
 add_action( 'wp_enqueue_scripts', 'aqueductmt_enqueue_styles' );
 function aqueductmt_enqueue_styles() {
 
@@ -12,5 +15,17 @@ function aqueductmt_enqueue_styles() {
 		get_stylesheet_directory_uri() . '/style.css',
 		array( $parent_style ),
 		wp_get_theme()->get('Version')
-    );
+	);
+}
+
+
+/*
+ * Deactive sidebar for mobile devices.
+ */
+add_filter( 'is_active_sidebar', 'aqueductmt_is_active_sidebar' );
+function aqueductmt_is_active_sidebar($is_active_sidebar ) {
+	if (wp_is_mobile()) {
+		return false;
+	}
+	return $is_active_sidebar;
 }
