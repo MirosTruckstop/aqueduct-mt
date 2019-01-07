762b9a49416c8286079302d7b939fb65aee843ad
Xennis <code@xennis.org>
2019-01-08 00:12:04 +0100

    Merge Gulp code into single file

--- gulp/tasks/watch.js
+++
@@ -1,8 +0,0 @@
-/*global require, module, __dirname */
-var config = require('../config').watch,
-	path = require('path'),
-	gulp = require('gulp');
-
-gulp.task('watch', function () {
-	gulp.watch(config.styles, ['styles']);
-});
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,26 @@
-var requireDir = require('require-dir');
+/*global require, module, __dirname */
+var gulp = require('gulp'),
+	cleanCss = require('gulp-clean-css'),
+	less = require('gulp-less'),
+	rename = require('gulp-rename'),
+	path = require('path');
 
-requireDir('./gulp/tasks', { recurse: true });
+gulp.task('default', ['styles', 'watch']);
+
+gulp.task('watch', function () {
+	gulp.watch('src/less/*.less', ['styles']);
+});
+
+gulp.task('styles', function () {
+	return gulp.src('src/less/main.less')
+		.pipe(less({
+			paths: [ path.join(__dirname, 'less', 'includes') ]
+		}))
+		.pipe(rename({
+			basename: 'aqueduct-mt',
+			suffix: '.min'
+		}))
+		.pipe(cleanCss({compatibility: 'ie8'}))
+		.pipe(gulp.dest('dist/'))
+		;
+});
