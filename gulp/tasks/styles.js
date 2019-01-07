c9d0d71b04d1ffffb4e87640850d5b9bfd041973
Xennis <code@xennis.org>
2019-01-08 00:02:22 +0100

    Remove gulp-load-plugins

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -2,26 +2,30 @@
 var config = require('../config').styles,
 	path = require('path'),
 	gulp = require('gulp'),
-	$ = require('gulp-load-plugins')();
+	less = require('gulp-less'),
+	rename = require('gulp-rename'),
+	cleanCss = require('gulp-clean-css'),
+	util = require('gulp-util'),
+	notify = require('gulp-notify');
 
 gulp.task('styles', function () {
 	return gulp.src(config.src)
-		.pipe($.less({
+		.pipe(less({
 			paths: [ path.join(__dirname, 'less', 'includes') ]
 		}))
 		.on('error', handleError)
-		.pipe($.rename({
+		.pipe(rename({
 			basename: config.basename,
 			suffix: '.min'
 		}))
-		.pipe($.cleanCss({compatibility: 'ie8'}))
+		.pipe(cleanCss({compatibility: 'ie8'}))
 		.pipe(gulp.dest(config.dest))
 		;
 });
 
 var handleError = function (err) {
-	new $.util.log(err);
-	$.notify.onError({
+	new util.log(err);
+	notify.onError({
 		title: err.plugin+' error',
 		message: path.basename(err.filename)+" on line "+err.line+":"+err.column,
 		sound: false
