572ecfab233ce0ca7538c0707f3c58c30ce9f274
Xennis <code@xennis.org>
2019-01-07 23:27:52 +0100

    Fix PHP formatting

--- functions.php
+++ functions.php
@@ -5,19 +5,17 @@ setlocale(LC_ALL, get_locale().'.'.get_bloginfo('charset'));
 /**
  * Include styles and scripts.
  */
-add_action( 'wp_enqueue_scripts', 'aqueductmt_enqueue_styles' );
-function aqueductmt_enqueue_styles() {
-
+add_action('wp_enqueue_scripts', function () {
 	$parent_style = 'aqueduct';
 
-	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'aqueduct-mt',
+	wp_enqueue_style($parent_style, get_template_directory_uri().'/style.css');
+	wp_enqueue_style('aqueduct-mt',
 		get_stylesheet_directory_uri() . '/style.css',
 		array( $parent_style ),
 		wp_get_theme()->get('Version')
 	);
 
-	$colorSetting = get_theme_mod("color-setting");
+	$colorSetting = get_theme_mod('color-setting');
 	$customizedCss = <<<EOT
 #auswahl_leiste,
 .break-social {
@@ -47,16 +45,15 @@ a:active {
 	color: #fff !important;
 }
 EOT;
-	wp_add_inline_style( 'aqueduct-mt', $customizedCss);
-}
+	wp_add_inline_style('aqueduct-mt', $customizedCss);
+});
 
 /*
  * Deactive sidebar for mobile devices.
  */
-add_filter( 'is_active_sidebar', 'aqueductmt_is_active_sidebar' );
-function aqueductmt_is_active_sidebar($is_active_sidebar ) {
+add_filter('is_active_sidebar', function ($is_active_sidebar) {
 	if (wp_is_mobile()) {
 		return false;
 	}
 	return $is_active_sidebar;
-}
+});
